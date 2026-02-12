5e7998f9381480c72083b1ffc52a9e4f3759d431
malvikasingh-a <malvikasingh510@gmail.com>
2026-02-12 19:17:45 +0530

    Submitting Assignment 1

---
+++ assignment1.py
@@ -0,0 +1,20 @@
+#task:1 basic maths operations
+from os import name
+
+
+num1=float(input("Enter first number: "))
+num2=float(input("Enter second number: "))
+addition=num1+num2
+subtraction=num1-num2
+multiplication=num1*num2
+division=num1/num2
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+
+#task:2 
+first_name=input("Enter your first name:")
+last_name=input("Enter your last name: ")
+full_name=first_name+" "+last_name
+print("Hello,",full_name+"!Welcome to Python programming.")
